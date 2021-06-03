6591841145dce0a4ca469a0176de052744528cbc
eleftheros <eleftheros_all@protonmail.com>
2021-06-03 07:45:06 +0300

    Add loadNextSong function

--- scripts/app.js
+++ scripts/app.js
@@ -6,18 +6,23 @@ const play = document.getElementById("play");
 const prev = document.getElementById("prev");
 const next = document.getElementById("next");
 const songs = ["hey", "summer", "ukulele"];
+let currentSong = songs[0];
 
 // function to load a song
-const loadSong = song => {
+const loadSong = () => {
   //change src
-  let audioSrc = `./assets/${song}.mp3`;
+  let audioSrc = `./assets/${currentSong}.mp3`;
   audio.src = audioSrc;
 
+  //change title
+  title.textContent = currentSong;
+
   //change cover
-  let imgSrc = `./assets/${song}.jpg`;
+  let imgSrc = `./assets/${currentSong}.jpg`;
   cover.src = imgSrc;
 };
 
+// function to play song
 const playSong = () => {
   if(audio.paused) {
     musicContainer.classList.add("play");
@@ -29,8 +34,23 @@ const playSong = () => {
     audio.pause();
     play.innerHTML = `<i class="fas fa-play"></i>`;
   }
-}
+};
 
+//function to load next song
+const loadNextSong = () => {
+  let songIndex = songs.indexOf(currentSong);
+  let nextSongIndex = songIndex + 1;
+
+  //check if nextSongIndex is in array
+  let len = songs.length;
+  if(nextSongIndex === len)
+    currentSong = songs[0];
+  else
+    currentSong = songs[nextSongIndex];
+
+  loadSong();
+};
 
 // event listeners
 play.addEventListener("click", playSong);
+next.addEventListener("click", loadNextSong);
