f68322b29a81265970eb35f11d7fd8d17cf85627
eleftheros <eleftheros_all@protonmail.com>
2021-06-03 07:27:26 +0300

    Add playSong function

--- scripts/app.js
+++ scripts/app.js
@@ -2,6 +2,9 @@ const musicContainer = document.querySelector(".music-container");
 const title = document.getElementById("title");
 const audio = document.getElementById("audio");
 const cover = document.getElementById("cover");
+const play = document.getElementById("play");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
 const songs = ["hey", "summer", "ukulele"];
 
 // function to load a song
@@ -15,6 +18,19 @@ const loadSong = song => {
   cover.src = imgSrc;
 };
 
+const playSong = () => {
+  if(audio.paused) {
+    musicContainer.classList.add("play");
+    audio.play();
+    play.innerHTML = `<i class="fas fa-pause"></i>`;
+  }
+  else if(!audio.paused) {
+    musicContainer.classList.remove("play");
+    audio.pause();
+    play.innerHTML = `<i class="fas fa-play"></i>`;
+  }
+}
+
 
 // event listeners
-
+play.addEventListener("click", playSong);
